7358744138985560a0fe0042a87e8320a31f8e55
Durga <bhaskarbr407@gmail.com>
2024-07-06 19:50:04 +0530

    ansible

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,9 @@
 dnf install nginx -y
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-#some file needs to copy
 systemctl restart nginx
 systemctl enable nginx
